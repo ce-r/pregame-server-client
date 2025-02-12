8a4c2414c1b78301bf3023c08952d847cae0ba83
Carlos Rivas <cerivas@ucdavis.edu>
2025-02-12 15:43:28 -0700

    changes to client side

--- gnetworklibc/networking/servers/BasicClient.hpp
+++ gnetworklibc/networking/servers/BasicClient.hpp
@@ -11,21 +11,21 @@
 
 namespace gnetwork {
     class BasicClient {
-    private:
-        ConnectingSocket* cli_socket;
+        private:
+            ConnectingSocket* cli_socket;
 
-    protected:  // make these methods accessible to derived classes
-        virtual void writer() = 0;
-        virtual void print_buffer() = 0;
+        protected:  // make these methods accessible to derived classes
+            virtual void writer() = 0;
+            virtual void print_buffer() = 0;
 
-    public:
-        BasicClient(int domain, int service, int protocol, int port, uint32_t interface);
-        virtual ~BasicClient();
+        public:
+            BasicClient(int domain, int service, int protocol, int port, uint32_t interface);
+            virtual ~BasicClient();
 
-        virtual void claunch() = 0;  
-        virtual void launch() = 0;   
+            virtual void claunch() = 0;  
+            virtual void launch() = 0;   
 
-        ConnectingSocket* get_cli_socket() const;
+            ConnectingSocket* get_cli_socket() const;
     };
 }
 
